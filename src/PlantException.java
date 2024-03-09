7b978445cd912091b4f371f2bf65d1abdc9d9cd6
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-03-09 22:20:03 +0100

    Plant Class created - 3 constructors get methods 2 additional methods to return date of next watering day.

---
+++ src/PlantException.java
@@ -0,0 +1,13 @@
+
+
+//frekvence zálivky — pokud je parametrem 0 nebo záporné číslo,
+//systém vyhodí výjimku třídy PlantException s vhodným popisem.
+
+//data poslední zálivky — nesmí být starší než datum zasazení rostliny.
+
+public class PlantException extends Exception {
+
+
+
+
+}
