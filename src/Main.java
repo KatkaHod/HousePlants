14626b02f9c800d61a9e310b39c116a29bcd65e0
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-03-14 21:51:54 +0100

    added method - export to file and import from the file in Class listOfPlants.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import indoorPlants.ListOfPlants;
+import indoorPlants.Plant;
+import indoorPlants.PlantException;
+
+
 public class Main {
     public static void main(String[] args) {
+
+        final String OUTPUT_FILE = "kvetiny.txt";
 
 
 
--- src/indoorPlants/ListOfPlants.java
+++ src/indoorPlants/ListOfPlants.java
@@ -3,15 +3,54 @@ package indoorPlants;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*; //reading from the file
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
 
 
 public class ListOfPlants {
 
+    //atribut - provided value - Class Plant
     private List<Plant> plantsList = new ArrayList<>();
 
 
-    //export list of plant to file
-    public void exportToFile(String newFileListOfPlants) {
+    //add a plant to the list
+    public void addPlant(Plant newPlant) {
+        plantsList.add(newPlant);
+    }
+
+    //Add multiple plants at once
+    public void addPlants(List<Plant> plants){
+        this.plantsList.addAll(plants);
+    }
+
+    //get a plant through the index
+    public Plant getPlantViaIndex(int index) {
+        if (index >= 0 && index < plantsList.size()) {
+            return plantsList.get(index);
+        } else {
+            throw new IllegalArgumentException("Invalid index. Provided index: " + index);
+        }
+    }
+
+    //method for removing a specific plant list by index
+    public void removePlantViaIndex(int index) {
+        if (index >= 0 && index < plantsList.size()) {
+            plantsList.remove(index);
+        } else {
+            System.err.println("Invalid plant list index.");
+        }
+    }
+
+
+
+    //additional methods regarding export and import a file
+    // correct this
+
+    public void savePlantsToFile(String newFileListOfPlants) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(newFileListOfPlants))) {
             outputStream.writeObject(this);
             System.out.println("The plant list has been successfully exported to a file.");
@@ -20,23 +59,16 @@ public class ListOfPlants {
         }
     }
 
-
-    public static ListOfPlants importFromFile(String plants) {
+    public static ListOfPlants importPlantsFromFile(String plants) {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(plants))) {
             ListOfPlants plantList = (ListOfPlants) inputStream.readObject();
             System.out.println("The plant list has been successfully read from the file.");
             return plantList;
 
         } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error loading from file: " + e.getMessage());
+            System.err.println("Error loading from the file: " + e.getMessage());
             return null;
         }
-    }
-
-
-    //add a plant to the list
-    public void addPlant(Plant newPlant) {
-        plantsList.add(newPlant);
     }
 
 
