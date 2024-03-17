b6a1d53ce37ed76d3a06022a05c9a15a25f1ea33
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-03-17 20:43:01 +0100

    Added class Setting - constant for file

---
+++ src/indoorPlants/Settings.java
@@ -0,0 +1,9 @@
+package indoorPlants;
+
+public class Settings {
+    private static final String FILENAME = "resources/cart.txt";
+
+    public static String getFilename() {
+        return FILENAME;
+    }
+}
