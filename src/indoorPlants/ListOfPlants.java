971ba469663ec54ba17ec9ecc845bcffb6a184a7
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-03-19 17:54:38 +0100

    corrected methods in ListOfPlants

--- src/indoorPlants/ListOfPlants.java
+++ src/indoorPlants/ListOfPlants.java
@@ -17,7 +17,28 @@ public class ListOfPlants {
     private List<Plant> plantsList = new ArrayList<>();
 
 
-    //load /read from the text file
+    //load from txt. file
+    public void ReadContentFromFile(String fileName) throws PlantException {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine();
+                String[] parts = line.split("\t");
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+    }
 
 
 
