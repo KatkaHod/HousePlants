e94580f926ab376cde0186a8ca16bfdc458c7312
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-03-31 20:55:15 +0200

    Main - added methods working with listOfPlants

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import indoorPlants.ListOfPlants;
-import indoorPlants.PlantException;
-import indoorPlants.Settings;
+import indoorPlants.*;
 
+import java.time.LocalDate;
 import indoorPlants.ListOfPlants;
 import indoorPlants.ListOfPlants.*;
 
@@ -10,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         String fileName = Settings.getFilename();
-        System.out.println(Settings.getFilename());
+
 
 
         //load the kvetiny.txt file
@@ -23,13 +22,27 @@ public class Main {
                             + e.getLocalizedMessage()+"\n");
         }
 
-        //print the copy of the list
+
+        System.out.println("Load from the file 'kvetiny.txt'");
         System.out.println("\nList of plants: " + listOfPlants.getPlantsList() + "\n");
 
-        //add new two plants
+        System.out.println("add new two plants - Bromelie and bazalka into listOfPlants");
+        listOfPlants.addPlant(new Plant("Bromelie", "Tropical plant", LocalDate.of(2023,3,31), LocalDate.of(2023,3,31), 7));
+        listOfPlants.addPlant(new Plant("Bazalka","v kuchyni", LocalDate.of(2021,9,4), LocalDate.of(2021,9,4),3));
 
+        System.out.println("return to listOfPlants and check that two new plants have been added.");
+        System.out.println("\nList of plants: " + listOfPlants.getPlantsList() + "\n");
 
+        System.out.println("return plant via index");
+        System.out.println(listOfPlants.getPlantViaIndex(2));
 
+        System.out.println("remove plant via index. Index: 2");
+
+        try {
+        listOfPlants.removePlantViaIndex(2);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Plant can not be removed.\n" + e.getLocalizedMessage());
+        }
 
 
 
